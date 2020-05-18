c167eb7d4a4a8252a8ce2e4a53d334bdcdf1407e
maribelcuales <maribelcuales@gmail.com>
2020-05-18 23:50:19 +0800

    Refactored NasaPhotoData to a function component.

--- src/components/NasaPhotoData.js
+++ src/components/NasaPhotoData.js
@@ -1,8 +1,10 @@
 import React from "react"; 
 
-export default function NasaPhotoData() {
-  return 
+function NasaPhotoData() {
+  return (
 		<div className="container">
 			<h1>Nasa Photo of the Day</h1>
-		</div>;
-};
+		</div>
+	)};
+
+export default NasaPhotoData;
