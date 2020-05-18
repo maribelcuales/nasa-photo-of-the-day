2b2a20feda3e50847341a3507f481255de82d701
maribelcuales <maribelcuales@gmail.com>
2020-05-18 23:24:53 +0800

    Import and render NasaPhotoData component in App.js.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import NasaPhotoData from "./components/NasaPhotoData";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <NasaPhotoData />
     </div>
   );
 }
