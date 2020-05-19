d8d7fabd4f70defea4a035ba0744a1192f6415b7
maribelcuales <maribelcuales@gmail.com>
2020-05-20 00:00:16 +0800

    Added useEffect basic code to NasaPhotoData.

--- src/components/NasaPhotoData.js
+++ src/components/NasaPhotoData.js
@@ -2,6 +2,17 @@ import React, {useState, useEffect} from "react";
 
 function NasaPhotoData() {	
 	const [photo, setPhoto] = useState([]);
+
+	useEffect(() => {
+		axios
+		.get('')
+		.then(response => {
+			console.log(response);
+		})
+		.catch(error => {
+			console.log('there was an error', error)
+		});
+	}, []);
   return (
 		<div className="container">
 			<h1>Nasa Photo of the Day</h1>
