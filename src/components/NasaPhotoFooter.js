23f687aca18d6d548a7f399222be5a39d5d9af9d
maribelcuales <maribelcuales@gmail.com>
2020-05-20 21:35:41 +0800

    Created NasaPhotoFooter component with initial code.

---
+++ src/components/NasaPhotoFooter.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+
+const NasaPhotoFooter = props => {
+	return (
+		<div>
+			<img />
+			<span>National Aeronautics and Space Administration</span>
+			<span>NASA Official: Brian Dunbar</span>
+		</div>
+	);
+};
+
+export default NasaPhotoFooter; 
