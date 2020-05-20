a7e051d4958284d552ba9dbd8c3e1b25cc9f9ec0
maribelcuales <maribelcuales@gmail.com>
2020-05-20 21:29:23 +0800

    Creaed NasaPhotoGallery component with basic code.

---
+++ src/components/NasaPhotoGallery.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+
+const NasaPhotoGallery = props => {
+	return (
+		<div>
+			<img />
+			<button> See More </button>
+		</div>
+	);
+};
+
+export default NasaPhotoGallery; 
