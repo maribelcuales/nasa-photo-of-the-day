be8c0d68a79849d24d1e040638a72a1cb2612132
maribelcuales <maribelcuales@gmail.com>
2020-05-20 21:19:56 +0800

    Created NasaPhotoHeader component with basic code.

--- src/components/NasaPhotoData.js
+++ src/components/NasaPhotoData.js
@@ -15,7 +15,7 @@ function NasaPhotoData() {
 	}, []);
   return (
 		<div className="container">
-			<h1>Nasa Photo of the Day</h1>
+			{/* <h1>Nasa Photo of the Day</h1> */}
 		</div>
 	)};
 
---
+++ src/components/NasaPhotoHeader.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+
+const NasaPhotoHeader = props => {
+	return (
+		<div>
+			<img />
+			<h1>NASA Photo of the Day</h1>
+		</div>
+	);
+};
+
+export default NasaPhotoHeader; 
