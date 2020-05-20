d64de1f50cd8e919da75c4d0d04661101ec0c161
maribelcuales <maribelcuales@gmail.com>
2020-05-21 04:37:44 +0800

    Added NasaPhotoGallery component to NasaPhotoCard.

--- src/components/NasaPhotoCard.js
+++ src/components/NasaPhotoCard.js
@@ -1,9 +1,11 @@
 import React from "react";
+import NasaPhotoGallery from "./NasaPhotoGallery";
 
 const NasaPhotoCard = props => {
 		return
 			<div className="photo-card">
 				<img className="photo-of-the-day" src="" alt="nasa-photo-of-the-day" />
+				<NasaPhotoGallery /> 
 			</div>;  
 };
 
