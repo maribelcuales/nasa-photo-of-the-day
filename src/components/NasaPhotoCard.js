2c2498340a9726f96432d9af305bc78f3718115d
maribelcuales <maribelcuales@gmail.com>
2020-05-20 21:26:27 +0800

    Added className and alt to NasaPhotoCard component.

--- src/components/NasaPhotoCard.js
+++ src/components/NasaPhotoCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const NasaPhotoCard = props => {
 		return
 			<div className="photo-card">
-
+				<img className="photo-of-the-day" src="" alt="nasa-photo-of-the-day" />
 			</div>;  
 };
 
