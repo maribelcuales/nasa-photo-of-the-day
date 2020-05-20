1a6a9bd493d78c6aa071224f9cfb26e4dbd3323f
maribelcuales <maribelcuales@gmail.com>
2020-05-20 21:32:22 +0800

    Created NasaPhotoBottomNav component with initial code.

---
+++ src/components/NasaPhotoBottomNav.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+
+const NasaPhotoBottomNav = props => {
+	return (
+		<div>
+			<button> Photo of the Day </button>
+			<button> Gallery </button>
+			<button> Contact NASA </button>
+		</div>
+	);
+};
+
+export default NasaPhotoBottomNav; 
