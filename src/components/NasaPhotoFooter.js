f5fc43011002dd99a5a75989b6c30c5d2b047c2f
maribelcuales <maribelcuales@gmail.com>
2020-05-26 03:56:14 +0800

    Added image link to footer logo src.

--- src/components/NasaPhotoFooter.js
+++ src/components/NasaPhotoFooter.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const NasaPhotoFooter = props => {
 	return (
-		<div className="footer">
-			<img className="footer-logo" src="" alt="NASA Logo"/>
+		<div className="footer-div">
+			<img className="footer-logo" src="https://github.com/maribelcuales/nasa-photo-of-the-day/blob/master/assets/nasa-logo.png?raw=true" alt="NASA Logo"/>
 			<span>National Aeronautics and Space Administration</span>
 			<span>NASA Official: Brian Dunbar</span>
 		</div>
