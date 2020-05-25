36b940f972d55f023f8ceea2065c991911522e33
maribelcuales <maribelcuales@gmail.com>
2020-05-26 02:58:59 +0800

    Added className to div and src to header logo.

--- src/components/NasaPhotoHeader.js
+++ src/components/NasaPhotoHeader.js
@@ -1,9 +1,9 @@
-import React from "react"; 
+import React from "react";
 
 const NasaPhotoHeader = props => {
 	return (
-		<div>
-			<img className="header-logo" src="" alt="nasa-logo"/>
+		<div className="header-logo-div">
+			<img className="header-logo" src="https://github.com/maribelcuales/nasa-photo-of-the-day/blob/master/assets/nasa-logo.png?raw=true" alt="NASA Logo"/>
 			<h1>NASA Photo of the Day</h1>
 		</div>
 	);
