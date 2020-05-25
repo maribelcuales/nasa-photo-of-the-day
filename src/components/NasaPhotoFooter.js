c25c20bf3ddcb1f0610708aae47319b84d51f278
maribelcuales <maribelcuales@gmail.com>
2020-05-26 04:02:15 +0800

    Added divs to NasaPhotofooter.

--- src/components/NasaPhotoFooter.js
+++ src/components/NasaPhotoFooter.js
@@ -4,9 +4,13 @@ import "./styles.css";
 const NasaPhotoFooter = props => {
 	return (
 		<div className="footer-div">
-			<img className="footer-logo" src="https://github.com/maribelcuales/nasa-photo-of-the-day/blob/master/assets/nasa-logo.png?raw=true" alt="NASA Logo"/>
-			<span>National Aeronautics and Space Administration</span>
-			<span>NASA Official: Brian Dunbar</span>
+			<div className="footer-logo-div">
+				<img className="footer-logo" src="https://github.com/maribelcuales/nasa-photo-of-the-day/blob/master/assets/nasa-logo.png?raw=true" alt="NASA Logo"/>
+			</div>
+			<div className="footer-contact">
+				<span>National Aeronautics and Space Administration</span>
+				<span>NASA Official: Brian Dunbar</span>
+			</div>
 		</div>
 	);
 };
