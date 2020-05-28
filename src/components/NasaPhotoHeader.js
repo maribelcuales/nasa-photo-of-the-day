14b92a2515d9462a8ebca0e3db565205e4c3f002
maribelcuales <maribelcuales@gmail.com>
2020-05-29 00:16:50 +0800

    Replaced div with Jumbotron in photo header.

--- src/components/NasaPhotoHeader.js
+++ src/components/NasaPhotoHeader.js
@@ -1,12 +1,17 @@
 import React from "react";
+import {Jumbotron} from "reactstrap";
 import "./styles.css";
 
 const NasaPhotoHeader = props => {
 	return (
-		<div className="header-logo-div">
+		// <div className="header-logo-div">
+		// 	<img className="header-logo" src="https://github.com/maribelcuales/nasa-photo-of-the-day/blob/master/assets/nasa-logo.png?raw=true" alt="NASA Logo"/>
+		// 	<h1 className="title-header">NASA Photo of the Day</h1>
+		// </div>
+		<Jumbotron className="header-logo-div">
 			<img className="header-logo" src="https://github.com/maribelcuales/nasa-photo-of-the-day/blob/master/assets/nasa-logo.png?raw=true" alt="NASA Logo"/>
-			<h1 className="title-header">NASA Photo of the Day</h1>
-		</div>
+			<h1 className="display-3" >NASA Photo of the Day</h1>
+		</Jumbotron>
 	);
 };
 
