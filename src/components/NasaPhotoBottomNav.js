00fdb70a26d07c148cf307680ba81fd3b38dbbf0
maribelcuales <maribelcuales@gmail.com>
2020-05-30 01:42:20 +0800

    Used styled components to style button container in NasaPhotoBottomNav.

--- src/components/NasaPhotoBottomNav.js
+++ src/components/NasaPhotoBottomNav.js
@@ -1,13 +1,22 @@
 import React from "react"; 
 import { Button } from "reactstrap";
+import styled from "styled-components";
+import "./styles.css";
+
+const ButtonContainer = styled.div`
+	display: flex; 
+	width: 100%;
+	background-color: lightyellow;
+	justify-content: space-between;
+`
 
 const NasaPhotoBottomNav = props => {
 	return (
-		<Button>
+		<ButtonContainer>
 			<Button outline color="primary" href={props.hdUrl} target="_blank"> HD Resolution </Button>
 			<Button outline color="info" href="https://www.nasa.gov/multimedia/imagegallery/index.html" target="_blank"> NASA Image Gallery </Button>
 			<Button outline color="primary" href="https://www.nasa.gov" target="_blank"> NASA Official Website </Button>
-		</Button>
+		</ButtonContainer>
 	);
 };
 
