d20e41e9aac8e37675019180a95f5dd9af019eef
maribelcuales <maribelcuales@gmail.com>
2020-05-30 01:12:54 +0800

    Changed href link to NASA Image Gallery button.

--- src/components/NasaPhotoBottomNav.js
+++ src/components/NasaPhotoBottomNav.js
@@ -3,11 +3,11 @@ import { Button } from "reactstrap";
 
 const NasaPhotoBottomNav = props => {
 	return (
-		<div>
-			<Button outline color="info" > Photo of the Day </Button>
-			<Button outline color="secondary"> Gallery </Button>
-			<Button outline color="primary" href="https://www.nasa.gov" target="_blank"> Contact NASA </Button>
-		</div>
+		<Button>
+			<Button outline color="primary" href={props.HdUrl} target="_blank"> HD Resolution </Button>
+			<Button outline color="info" href="https://www.nasa.gov/multimedia/imagegallery/index.html" target="_blank"> NASA Image Gallery </Button>
+			<Button outline color="primary" href="https://www.nasa.gov" target="_blank"> NASA Official Website </Button>
+		</Button>
 	);
 };
 
