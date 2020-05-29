4dde5d11186e4bd9b390fe638951b369c689a231
maribelcuales <maribelcuales@gmail.com>
2020-05-29 16:58:25 +0800

    Import and used Reactstrap Button in NasaPhotoBottomNav.

--- src/components/NasaPhotoBottomNav.js
+++ src/components/NasaPhotoBottomNav.js
@@ -1,11 +1,12 @@
 import React from "react"; 
+import { Button } from "reactstrap";
 
 const NasaPhotoBottomNav = props => {
 	return (
 		<div>
-			<button> Photo of the Day </button>
-			<button> Gallery </button>
-			<button> Contact NASA </button>
+			<Button outline color="info" > Photo of the Day </Button>
+			<Button outline color="secondary"> Gallery </Button>
+			<Button outline color="primary" href="https://www.nasa.gov" target="_blank"> Contact NASA </Button>
 		</div>
 	);
 };
