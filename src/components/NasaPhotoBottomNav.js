aff33bb21fbab7fa1bb651110ff2af2311ffd88a
maribelcuales <maribelcuales@gmail.com>
2020-06-06 12:24:23 +0800

    Added a new button and css styling.

--- src/components/NasaPhotoBottomNav.js
+++ src/components/NasaPhotoBottomNav.js
@@ -7,13 +7,17 @@ const ButtonContainer = styled.div`
 	display: inline-block;
 	width: 80%;
 	padding-bottom: 40px;
+	align-items: center;
+	justify-content: space-between;
 `
+
 const NasaPhotoBottomNav = props => {
 	return (
-		<ButtonContainer>
+		<ButtonContainer className="button-container">
 			<Button className="button1-style" outline color="primary" href={props.hdUrl} target="_blank"> HD Resolution </Button>
 			<Button className="button2-style" outline color="info" href="https://www.nasa.gov/multimedia/imagegallery/index.html" target="_blank"> NASA Image Gallery </Button>
 			<Button className="button3-style" outline color="primary" href="https://www.nasa.gov" target="_blank"> NASA Official Website </Button>
+			<Button className="button4-style" outline color="warning" href="https://www.nasa.gov/nasa-at-home-be-a-scientist" target="_blank"> Be a Scientist! </Button>
 		</ButtonContainer>
 	);
 };
