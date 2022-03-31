39db2fc59b04a7ee8f4b726e4bbfc5cf7a7324fe
maribelcuales <maribelcuales@gmail.com>
2022-03-31 21:50:51 +0800

    Deleted unused code and add styling to h1.

--- src/components/NasaPhotoHeader.js
+++ src/components/NasaPhotoHeader.js
@@ -1,19 +1,17 @@
-import React from "react";
-import {Jumbotron} from "reactstrap";
+import React from "react"
 import "./styles.css";
 
-const NasaPhotoHeader = props => {
-	return (
-		// <div className="header-logo-div">
-		// 	<img className="header-logo" src="https://github.com/maribelcuales/nasa-photo-of-the-day/blob/master/assets/nasa-logo.png?raw=true" alt="NASA Logo"/>
-		// 	<h1 className="title-header">NASA Photo of the Day</h1>
-		// </div>
-
-		<Jumbotron className="jumbotron-div">
-			<img className="header-logo" src="https://github.com/maribelcuales/nasa-photo-of-the-day/blob/master/assets/nasa-logo.png?raw=true" alt="NASA Logo"/>
-			<h1 className="display-4" >Astronomy Photo of the Day</h1>
-		</Jumbotron>
-	);
+const NasaPhotoHeader = (props) => {
+  return (
+    <div className="header-logo-div">
+      <img
+        className="header-logo"
+        src="https://github.com/maribelcuales/nasa-photo-of-the-day/blob/master/assets/nasa-logo.png?raw=true"
+        alt="NASA Logo"
+      />
+      <h1 className="title-header">Astronomy Photo of the Day</h1>
+    </div>
+  );
 };
 
-export default NasaPhotoHeader; 
+export default NasaPhotoHeader;
